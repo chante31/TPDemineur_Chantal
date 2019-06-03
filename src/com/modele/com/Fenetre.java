59d6c16527cfef5c13dc455037d0011ffef7abe7
elya01 <44399210+elya01@users.noreply.github.com>
2019-06-03 15:40:32 +0200

    Update Fenetre.java

--- src/com/modele/com/Fenetre.java
+++ src/com/modele/com/Fenetre.java
@@ -9,15 +9,21 @@ import javax.swing.JPanel;
 import com.modele.com.Fenetre.Grille;
 
 public class Fenetre extends JFrame {
-//ceci est un commentaire d'essai	
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+int spacing=5;
+
+	//ceci est un commentaire d'essai	
 	public Fenetre() {
 		this.setTitle("DEMINEUR");
-		this.setSize(1200,800);
+		this.setSize(1200,850);
 		 //Instanciation d'un objet JPanel
         JPanel couleur = new JPanel();
  
         //Définition de la couleur de fond
-        couleur.setBackground(Color.DARK_GRAY);  
+        //couleur.setBackground(Color.DARK_GRAY);  
  
         //On informe  notre JFrame que notre JPanel qui sera son contentPane
         this.setContentPane(couleur);
@@ -27,15 +33,29 @@ public class Fenetre extends JFrame {
 
 
 				
-		//Grille grille =new Grille();
-		//this.setContentPane(grille);//
+		Grille grille =new Grille();
+		this.setContentPane(grille);//
 	}
 
 
 	public class Grille extends JPanel{
-		public void grille(Graphics g) {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		public void paintComponent(Graphics g) {
 			g.setColor(Color.red);
-			g.fillRect(87, 348, 69, 234);
+			g.fillRect(0, 0, 1200, 800);
+			g.setColor(Color.DARK_GRAY);
+			for (int i =0;i<16; i++) {
+				for (int j=0 ; j<9; j++) {
+					
+					g.fillRect(spacing +i*80,spacing +j*80+80, 80-2*spacing,80-2*spacing);
+				}
+			}
+			
+			
 			
 		}
 		
